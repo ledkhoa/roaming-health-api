8ee214cb7bba75f1bba508a3502ed103a2b39fce
Khoa Le <kle815@outlook.com>
2025-03-09 11:09:36 -0700

    add workers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WorkersModule } from './workers/workers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WorkersModule],
 })
 export class AppModule {}
