f516f351bc3914a9f2726dbcce5071fc5c0ec54b
Khoa Le <kle815@outlook.com>
2025-03-18 23:08:49 -0700

    update e2e tests

--- test/workers.e2e-spec.ts
+++ test/workers.e2e-spec.ts
@@ -9,6 +9,8 @@ import { drizzle, PgliteDatabase } from 'drizzle-orm/pglite';
 import * as schema from 'src/drizzle/schema';
 import { DrizzleService } from 'src/drizzle/drizzle.service';
 import { migrate } from 'drizzle-orm/pglite/migrator';
+import { eq } from 'drizzle-orm';
+import { faker } from '@faker-js/faker';
 
 describe('Workers (e2e)', () => {
   let app: INestApplication<App>;
@@ -47,18 +49,41 @@ describe('Workers (e2e)', () => {
   describe('POST', () => {
     it('should create a Worker', () => {
       const worker: CreateWorkerDto = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@email.com',
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
       };
 
       return request(app.getHttpServer())
         .post('/workers')
         .send(worker)
         .expect(201)
-        .expect((response) => {
-          expect(response.body).toMatchObject({ ...worker, isActive: true });
+        .expect(async (response) => {
+          expect(response.body.id).toBeDefined();
+          const workerId = response.body.id;
+
+          const record = await db.query.WorkersTable.findFirst({
+            where: eq(schema.WorkersTable.id, workerId),
+          });
+          expect(record).toMatchObject({
+            ...worker,
+            id: workerId,
+            isActive: true,
+          });
         });
+    });
+
+    // TODO handle error in api
+    it('should fail to create a Worker', () => {
+      const worker = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      };
+
+      return request(app.getHttpServer())
+        .post('/workers')
+        .send(worker)
+        .expect(500);
     });
   });
 
@@ -71,11 +96,13 @@ describe('Workers (e2e)', () => {
     });
 
     it('should return 1 worker', async () => {
-      await db.insert(schema.WorkersTable).values({
-        firstName: 'jayne',
-        lastName: 'doe',
-        email: 'jaynedoe@email.com',
-      });
+      const worker: CreateWorkerDto = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+      };
+
+      await db.insert(schema.WorkersTable).values(worker);
 
       return request(app.getHttpServer())
         .get('/workers')
@@ -83,9 +110,7 @@ describe('Workers (e2e)', () => {
         .expect((response) => {
           expect(response.body.data).toMatchObject([
             {
-              firstName: 'jayne',
-              lastName: 'doe',
-              email: 'jaynedoe@email.com',
+              ...worker,
               isActive: true,
             },
           ]);
@@ -93,14 +118,95 @@ describe('Workers (e2e)', () => {
         });
     });
 
-    // it('should get worker by id', () => {
-    //   const id = 'df35af14-f973-4552-9363-4297155418c4';
+    it('should get worker by id', async () => {
+      const worker: CreateWorkerDto = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+      };
 
-    //   const result = request(app.getHttpServer()).get(`/workers/${id}`);
-    //   return result.expect(200).expect((response) => {
-    //     expect(response.body).toEqual({});
-    //   });
-    // });
+      const created = await db
+        .insert(schema.WorkersTable)
+        .values(worker)
+        .returning({ id: schema.WorkersTable.id });
+
+      const id = created[0].id;
+
+      return request(app.getHttpServer())
+        .get(`/workers/${id}`)
+        .expect(200)
+        .expect((response) => {
+          expect(response.body).toMatchObject({
+            ...worker,
+            id,
+            isActive: true,
+          });
+        });
+    });
+
+    it('should fail to get worker by id', () => {
+      const id = faker.string.uuid();
+
+      return request(app.getHttpServer()).get(`/workers/${id}`).expect(404);
+    });
+  });
+
+  describe('PATCH', () => {
+    it('should update a worker', async () => {
+      const worker: CreateWorkerDto = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+      };
+
+      const created = await db
+        .insert(schema.WorkersTable)
+        .values(worker)
+        .returning({ id: schema.WorkersTable.id });
+
+      const id = created[0].id;
+
+      const updatedEmail = faker.internet.email();
+
+      return await request(app.getHttpServer())
+        .patch(`/workers/${id}`)
+        .send({ ...worker, email: updatedEmail })
+        .expect(200)
+        .expect(async (_) => {
+          const records = await db
+            .select({ email: schema.WorkersTable.email })
+            .from(schema.WorkersTable)
+            .where(eq(schema.WorkersTable.id, id));
+          const recordEmail = records[0].email;
+
+          expect(recordEmail).toBe(updatedEmail);
+        });
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should delete a worker', async () => {
+      const worker: CreateWorkerDto = {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+      };
+
+      const created = await db
+        .insert(schema.WorkersTable)
+        .values(worker)
+        .returning({ id: schema.WorkersTable.id });
+
+      const id = created[0].id;
+
+      return request(app.getHttpServer()).delete(`/workers/${id}`).expect(204);
+    });
+
+    it('should fail to delete a worker', () => {
+      const id = faker.string.uuid();
+
+      return request(app.getHttpServer()).delete(`/workers/${id}`).expect(404);
+    });
   });
 
   afterAll(async () => {
